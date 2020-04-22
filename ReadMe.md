1e9242d2dc67648327a0c8e992804f6057a81dbc
Federico Barresi <fede.barresi@gmail.com>
2020-04-22 21:38:12 +0200

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+﻿## Summary
+install painless the anyconnect-win-3.1.06073-pre-deploy-k9
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'anyconnect-win-3.1.06073-pre-deploy-k9.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi' #only one of these: exe, msi, msu
+  file         = $fileLocation
+
+  softwareName  = 'CiscoAnyConnect*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  silentArgs   = '/quiet'
+}
+
+Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
